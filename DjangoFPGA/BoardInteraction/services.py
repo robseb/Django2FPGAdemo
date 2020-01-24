0b7b4fd10e304ea9ca48bc9b6ca2c760d0e21bda
Robin Sebastian <mail@robseb.de>
2020-01-24 13:51:55 +0100

    Update services.py

--- DjangoFPGA/BoardInteraction/services.py
+++ DjangoFPGA/BoardInteraction/services.py
@@ -1,3 +1,6 @@
+'''
+    Django FPGA Interaction demo application  - "sevices.py"
+'''
 import subprocess
 
 from BoardInteraction.models import ADCSensorReading, ADCchannel 
@@ -5,19 +8,23 @@ from BoardInteraction.models import ADCSensorReading, ADCchannel
 # For accessing the the Project settings
 from django.conf import settings
 
-
-# Singe shoot ADC Channel and write the value to the mySQLite Database 
+#
+# Start a python application to read the ADC value 
+# and write the value to a database
+#
 def ReadADCchannel():
+    # For every activ ADC Channel 
     for sensor in ADCchannel.objects.all():   
         adc_u = 0
+        # Start the Python Script "adcReadChannel.py" to read the ADC Channel
         try: 
             adc_u = float(subprocess.check_output(['python3',settings.BASE_DIR+'/BoardInteraction/adcReadChannel.py',str(sensor.ch)],stderr=subprocess.STDOUT, timeout=None))
         except ValueError:
             adc_u = 0
             subprocess.call('echo Value Error ', shell=True)
         
+        # Write the value to the database
         ADCSensorReading.reading = adc_u 
-        subprocess.call('echo '+str(adc_u), shell=True)
         
         newreading = ADCSensorReading(reading= ADCSensorReading.reading)
         newreading.save()
